e23e6ed7f02a529e87d246900441aeb306f80a1c
mengyingiris <60266182+mengyingiris@users.noreply.github.com>
2021-04-22 11:21:38 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+let canvas;
+function setup() {
+  // put setup code here
+  canvas= createCanvas(400,600);
+  canvas.position((windowWidth/2-200),(windowHeight/2-300));
+  background(50);
+
+}
+
+function draw() {
+  // put drawing code here
+}
